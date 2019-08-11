a4e7492387d4d38a92ed4ec72d35140200a1f9d3
Cyrus <david@jcyr.us>
2019-08-11 09:50:13 +0800

    service worker

--- next.config.js
+++ next.config.js
@@ -1,5 +1,27 @@
 const withCSS = require('@zeit/next-css')
+const withOffline = require('next-offline')
 
-module.exports = withCSS({
-  target: 'serverless'
-})
+module.exports = withOffline(withCSS({
+  target: 'serverless',
+  transformManifest: manifest => ['/'].concat(manifest), // Add the homepage to the cache
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
+  }
+}))
