5d00cf71cef28e45defc6d46f89b9aa0da0ccd23
Cyrus <david@jcyr.us>
2019-08-11 10:18:30 +0800

    add pwa-related meta tags

--- pages/index.js
+++ pages/index.js
@@ -41,6 +41,8 @@ class Home extends React.Component {
           <title>Gym Timer</title>
           <meta name="viewport" content="width=device-width, initial-scale=1"/>
           <link rel="manifest" href="/static/manifest.json"/>
+          <meta name="theme-color" content="#FFFFFF"/>
+          <link rel="apple-touch-icon" href="/static/icon-512.png"/>
           <meta charset="utf-8"/>
         </Head>
         <Provider store={store}>
